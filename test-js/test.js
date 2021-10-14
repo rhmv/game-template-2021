11cf1f3893247e02bf837a69023de9883fe0b53f
rhmv <87086212+rhmv@users.noreply.github.com>
2021-10-14 21:48:14 +0400

    Add files via upload

---
+++ test-js/test.js
@@ -0,0 +1,18 @@
+// Swiper 1
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
